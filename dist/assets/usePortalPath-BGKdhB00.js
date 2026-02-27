471f276ff6fce75607b98df70de2a71abfa74d97
auxxweb <auxxweb@gmail.com>
2026-02-27 14:46:49 +0530

    bugs image upload resolved 1

---
+++ dist/assets/usePortalPath-BGKdhB00.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-DmDksVhj.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
--- dist/assets/usePortalPath-CsSYngOC.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-BZTLDehx.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
