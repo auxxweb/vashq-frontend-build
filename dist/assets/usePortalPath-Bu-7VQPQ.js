b6a018f125c75ac72cbd1453e3212aaa1423e1ba
auxxweb <auxxweb@gmail.com>
2026-02-27 16:49:01 +0530

    bugs image upload resolved 5

--- dist/assets/usePortalPath-Bu-7VQPQ.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-o_2gjTLu.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
---
+++ dist/assets/usePortalPath-D-dbO4zZ.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-iCWoet6q.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
