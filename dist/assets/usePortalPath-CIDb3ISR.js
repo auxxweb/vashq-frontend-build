dbc306d5eb8c3a64073ed736e6f7ed2e7aef2532
auxxweb <auxxweb@gmail.com>
2026-02-27 23:16:00 +0530

    image upload

--- dist/assets/usePortalPath-CIDb3ISR.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-DqxcJu2F.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
---
+++ dist/assets/usePortalPath-kWBXknMv.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-D5eo3nMI.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
