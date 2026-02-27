1ced5959968d23f1d7e34760f4e7b6eaccdee034
auxxweb <auxxweb@gmail.com>
2026-02-27 16:12:09 +0530

    bugs image upload resolved 3

--- dist/assets/usePortalPath-CKaWAX-Z.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-3RdFwQhj.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
---
+++ dist/assets/usePortalPath-fDGWwlGx.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-BW20lZeO.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
