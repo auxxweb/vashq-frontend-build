f5e030e20698205766258312b52ba6db400bd05c
auxxweb <auxxweb@gmail.com>
2026-03-11 14:36:40 +0530

    ready expense repost payment type module 1

---
+++ dist/assets/usePortalPath-DGWYU3XY.js
@@ -0,0 +1,1 @@
+import{D as e}from"./index-BZAb20nH.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
--- dist/assets/usePortalPath-cwrlfGxE.js
+++
@@ -1,1 +0,0 @@
-import{D as e}from"./index-BkBO-9-j.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
