dbc1c59cb520a59dfc9e3ae1e16d44c256f6a768
auxxweb <auxxweb@gmail.com>
2026-03-17 23:39:59 +0530

    bugs number , image upload non mandotory, local host invoice

---
+++ dist/assets/usePortalPath-Bkr2zk9v.js
@@ -0,0 +1,1 @@
+import{D as e}from"./index-3ew7Ww_r.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
--- dist/assets/usePortalPath-DGWYU3XY.js
+++
@@ -1,1 +0,0 @@
-import{D as e}from"./index-BZAb20nH.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
