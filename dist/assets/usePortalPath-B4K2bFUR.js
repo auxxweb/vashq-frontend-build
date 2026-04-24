98403889a1bc1911295afb36209390ffd326da6a
auxxweb <auxxweb@gmail.com>
2026-04-24 18:02:07 +0530

    added payment type on job cars

--- dist/assets/usePortalPath-B4K2bFUR.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./index-BwYIyYtk.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
---
+++ dist/assets/usePortalPath-D0EjsJac.js
@@ -0,0 +1,1 @@
+import{S as e}from"./index-9LyGRgYI.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
