072c1d3f1e63665e7ff97440d7276058682aecda
auxxweb <auxxweb@gmail.com>
2026-04-25 03:26:06 +0530

    test 1

--- dist/assets/usePortalPath-Dj3G6kaz.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./index-D6hj727I.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
---
+++ dist/assets/usePortalPath-rUaenQUc.js
@@ -0,0 +1,1 @@
+import{S as e}from"./index-BaAiBIzL.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
