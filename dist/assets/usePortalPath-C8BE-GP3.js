190de773d1d24862f4fcf9fe2ffe46dd737687b2
auxxweb <auxxweb@gmail.com>
2026-04-27 18:26:05 +0530

    new features added package

---
+++ dist/assets/usePortalPath-C8BE-GP3.js
@@ -0,0 +1,1 @@
+import{$ as e}from"./index-BaPCiDv4.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
--- dist/assets/usePortalPath-rUaenQUc.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./index-BaAiBIzL.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
