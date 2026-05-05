7eef3faa03fbaa788ff67eac05b61e039d7dfb6a
auxxweb <auxxweb@gmail.com>
2026-05-05 13:04:51 +0530

    new changes made , advance payment , split options added and bugs resolved

--- dist/assets/usePortalPath-C8BE-GP3.js
+++
@@ -1,1 +0,0 @@
-import{$ as e}from"./index-BaPCiDv4.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
---
+++ dist/assets/usePortalPath-DDva-SCX.js
@@ -0,0 +1,1 @@
+import{a0 as e}from"./index-B1axv2JU.js";function o(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{o as u};
