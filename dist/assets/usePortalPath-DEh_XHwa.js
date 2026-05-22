3da1cbbc47a385e5aa59c7099a5c7b4ef6ae5175
auxxweb <auxxweb@gmail.com>
2026-05-22 19:46:50 +0530

    service filter bug solved

--- dist/assets/usePortalPath-DEh_XHwa.js
+++
@@ -1,1 +0,0 @@
-import{a0 as e}from"./index-CKvEcKZa.js";function o(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{o as u};
---
+++ dist/assets/usePortalPath-DP-WTkxR.js
@@ -0,0 +1,1 @@
+import{a0 as e}from"./index-BzmH462l.js";function o(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{o as u};
