4eb29c06f00f2637dddf166fa1ffee7a65090450
auxxweb <auxxweb@gmail.com>
2026-06-19 04:25:27 +0530

    new fatures added , ai , accounting, booking and crm

---
+++ dist/assets/brandTheme-CO5w4aad.js
@@ -0,0 +1,1 @@
+const R="#007BFF",s="211 100% 50%",a=`hsl(${s})`;export{R as B,a as b};
--- dist/assets/usePortalPath-D9XwvQEw.js
+++
@@ -1,1 +0,0 @@
-import{H as e}from"./index-BZKu0mQj.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
