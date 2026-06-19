e240068161574ca23f36c2b2749fabaaf67346dd
auxxweb <auxxweb@gmail.com>
2026-06-19 05:33:28 +0530

    new fatures added , ai , accounting, booking and crm 4

--- dist/assets/usePortalPath-DFMJIulN.js
+++
@@ -1,1 +0,0 @@
-import{a8 as e}from"./index-BD9dJbK-.js";function o(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{o as u};
---
+++ dist/assets/usePortalPath-P1WZNp9X.js
@@ -0,0 +1,1 @@
+import{a8 as e}from"./index-puYDZ5wX.js";function o(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{o as u};
