38eb4844a3ce0dff80c4ee06041dd0f5fff6deec
auxxweb <auxxweb@gmail.com>
2026-07-01 03:08:46 +0530

    multibranch and other features updated 5

--- dist/assets/usePortalPath-CypeIX7b.js
+++
@@ -1,1 +0,0 @@
-import{ah as e}from"./index-DPBu_G_M.js";function o(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{o as u};
---
+++ dist/assets/usePortalPath-DBJ8PMFX.js
@@ -0,0 +1,1 @@
+import{ah as e}from"./index-0TFLd0Ip.js";function o(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{o as u};
