a15a059a0543a1356c831bc3ca1a84c45f124818
auxxweb <auxxweb@gmail.com>
2026-07-03 18:56:26 +0530

    beta bugs resolved

---
+++ dist/assets/useBranchScopedEffect-C8P6Clq5.js
@@ -0,0 +1,1 @@
+import{r as t}from"./motion-Bp917_L6.js";import{M as c}from"./index-CNYTAMPo.js";function o(){const e=c();return e?e.scope==="all"?"all":e.activeBranchId||"none":"none"}function u(e,r=[]){const n=o();t.useEffect(()=>e(),[n,...r])}export{u as a,o as u};
