2b6d1182547f8a40dd509128cf8f77b32e0fa440
auxxweb <auxxweb@gmail.com>
2026-07-08 22:45:54 +0530

    final production 8

---
+++ dist/assets/usePortalPath-2wUH9jx0.js
@@ -0,0 +1,1 @@
+import{ak as e}from"./index-ClF4BVuf.js";function o(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{o as u};
--- dist/assets/usePortalPath-QHbyc7kK.js
+++
@@ -1,1 +0,0 @@
-import{ak as e}from"./index-BXqxZR4R.js";function o(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{o as u};
