d79fdee2e7759ca489399765f51df69653ff6aa4
auxxweb <auxxweb@gmail.com>
2026-07-08 22:04:36 +0530

    final production 6

--- dist/assets/usePortalPath-ChBjyYDo.js
+++
@@ -1,1 +0,0 @@
-import{ak as e}from"./index-CC6duak9.js";function o(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{o as u};
---
+++ dist/assets/usePortalPath-iNgCZK9F.js
@@ -0,0 +1,1 @@
+import{ak as e}from"./index-C-8lOZli.js";function o(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{o as u};
