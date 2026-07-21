265f4efeb08c79c329d2be04d089975a4de2f205
auxxweb <auxxweb@gmail.com>
2026-07-21 16:58:54 +0530

    cochin wash club changes and nexus changes completed

---
+++ dist/assets/usePortalPath-B9mcg-OZ.js
@@ -0,0 +1,1 @@
+import{ao as e}from"./index-DueBjXSm.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
--- dist/assets/usePortalPath-BqkXTtqg.js
+++
@@ -1,1 +0,0 @@
-import{ao as e}from"./index-DHqnzLNn.js";function a(){const{pathname:t}=e();return t.startsWith("/employee")?"/employee":"/admin"}export{a as u};
