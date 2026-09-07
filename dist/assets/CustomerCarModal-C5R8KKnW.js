19e41a5d3cc320e1255c7e02b3d5bf498b0faa7b
auxxweb <auxxweb@gmail.com>
2026-09-07 13:43:27 +0530

    CashBank Ledger Isssue solved

---
+++ dist/assets/CustomerCarModal-C5R8KKnW.js
@@ -0,0 +1,1 @@
+import"./motion-Bp917_L6.js";import"./index-DWYukM8p.js";import"./VehicleNumberInput-MU3QQXOw.js";import"./select-BfkjQ6fL.js";function n(o){if(!o)return"";const r=[o.brand,o.model,o.color].filter(Boolean).join(" · ");return r?`${o.carNumber} · ${r}`:o.carNumber}export{n as f};
